ecae7215e0679fcd6578a96a35986280313a0f6c
Eero Helenius <eero.helenius@solita.fi>
2017-09-19 22:28:19 +0300

    Consolidate extend-protocol calls

--- src/sigel/protocols.clj
+++ src/sigel/protocols.clj
@@ -13,52 +13,47 @@
   [XdmNode](http://www.saxonica.com/html/documentation/javadoc/net/sf/saxon/s9api/XdmNode.html)."
   (build [source]))
 
-(extend-protocol XMLSource XdmNode (build [xdmnode] xdmnode))
-
-(extend-protocol XMLSource String
-                 (build [xml-string]
-                   (.build saxon/builder (StreamSource. (StringReader. xml-string)))))
-
-(extend-protocol XMLSource File
-                 (build [file]
-                   (.build saxon/builder file)))
-
-(extend-protocol XMLSource Source
-                 (build [source]
-                   (.build saxon/builder source)))
-
-(extend-protocol XMLSource URI
-                 (build [^URI uri]
-                   (.build saxon/builder (StreamSource. uri))))
-
-(extend-protocol XMLSource URL
-                 (build [^URL url]
-                   (.build saxon/builder (StreamSource. (.toString url)))))
+(extend-protocol XMLSource
+  XdmNode
+  (build [xdmnode] xdmnode)
+  String
+  (build [xml-string]
+    (.build saxon/builder (StreamSource. (StringReader. xml-string))))
+  File
+  (build [file]
+    (.build saxon/builder file))
+  Source
+  (build [source]
+    (.build saxon/builder source))
+  URI
+  (build [^URI uri]
+    (.build saxon/builder (StreamSource. uri)))
+  URL
+  (build [^URL url]
+    (.build saxon/builder (StreamSource. (.toString url)))))
 
 (defprotocol QNameable
   "A protocol for things that can be converted into a
   [QName](http://www.saxonica.com/html/documentation/javadoc/net/sf/saxon/s9api/QName.html)."
   (->qname [name]))
 
-(extend-protocol QNameable QName
-                 (->qname [qname] qname))
-
-(extend-protocol QNameable String
-                 (->qname [qname] (QName. qname)))
-
-(extend-protocol QNameable Keyword
-                 (->qname [qname] (-> qname name ->qname)))
-
-(extend-protocol QNameable APersistentVector
-                 (->qname [[namespace name]] (QName. namespace name)))
+(extend-protocol QNameable
+  QName
+  (->qname [qname] qname)
+  String
+  (->qname [qname] (QName. qname))
+  Keyword
+  (->qname [qname] (-> qname name ->qname))
+  APersistentVector
+  (->qname [[namespace name]] (QName. namespace name)))
 
 (defprotocol XMLValue
   "A protocol for things that can be converted into a Saxon
   [XdmValue](http://www.saxonica.com/html/documentation/javadoc/net/sf/saxon/s9api/XdmValue.html)."
   (->xdmvalue [value]))
 
-(extend-protocol XMLValue XdmValue
-                 (->xdmvalue [xdmvalue] xdmvalue))
-
-(extend-protocol XMLValue Object
-                 (->xdmvalue [obj] (XdmAtomicValue. obj)))
+(extend-protocol XMLValue
+  XdmValue
+  (->xdmvalue [xdmvalue] xdmvalue)
+  Object
+  (->xdmvalue [obj] (XdmAtomicValue. obj)))
